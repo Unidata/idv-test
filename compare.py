75e81920ad910195c4ed37f9ec1e855155f8879c
Julien Chastang <julien.c.chastang@gmail.com>
2015-05-08 17:16:01 -0600

    pylint

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-import math, operator
+import math
 from pyhiccup.core import html
 from html5print import HTMLBeautifier
 from os import listdir
@@ -11,10 +11,11 @@ TEST_PATH = IDV_PATH + "test-output/"
 BUNDLE_PATH = IDV_PATH + "idv-test/bundles/"
 BASE_PATH = IDV_PATH + "test-output/baseline/"
 
+
 # http://code.activestate.com/recipes/577630-comparing-two-images/
 def rmsdiff(i1, i2):
     "Calculate the root-mean-square difference between two images"
-    if (isfile(i1) and isfile(i2)): 
+    if (isfile(i1) and isfile(i2)):
         im1, im2 = Image.open(i1), Image.open(i2)
         diff = ImageChops.difference(im1, im2)
         h = diff.histogram()
@@ -25,11 +26,12 @@ def rmsdiff(i1, i2):
     else:
         return 0
 
+
 def readoutput(file):
     "Read file intro string"
     data = 'EMPTY ' + file
     if (isfile(file)):
-        with open (file, "r") as myfile:
+        with open(file, "r") as myfile:
             data = myfile.read()
     return data
 
@@ -37,19 +39,19 @@ def readoutput(file):
 def process(bundle):
     "Process the test output of the given bundle"
     bname = splitext(basename(bundle))[0]
-    result =[("./baseline/" + f,
-             "./results/" + f,
-             rmsdiff(TEST_PATH + "baseline/" + f, TEST_PATH + "results/" + f))
-        for f in listdir(BASE_PATH) if f.startswith(bname + "_view")]
+    result = [("./baseline/" + f,
+               "./results/" + f,
+               rmsdiff(TEST_PATH + "baseline/" + f,
+                       TEST_PATH + "results/" + f))
+              for f in listdir(BASE_PATH) if f.startswith(bname + "_view")]
     return result
 
-    
 baseline = [(f,
              readoutput(TEST_PATH + "results/" + splitext(f)[0] + ".xidv.out"),
              process(f))
-             for f in listdir(BUNDLE_PATH) if isfile(join(BUNDLE_PATH,f)) ]
+            for f in listdir(BUNDLE_PATH) if isfile(join(BUNDLE_PATH, f))]
 
-# sorting on first img, even if there are multiple images/bundle    
+# sorting on first img, even if there are multiple images/bundle
 sortimgs = sorted(baseline, key=lambda img: img[2][0][2])
 
 
@@ -57,18 +59,18 @@ data = [[
         ['hr'],
         ['h1', x[0]],
         ['table',
-        ['tr', ['td', x[1].replace("\n","<br />")]],
-        ['tr', [['td',['h2', "expected"]],['td',['h2', "actual"]]]], 
-        [['tr',
-          [['td', ['img', {'src' : i[0]}]],
-           ['td', ['img', {'src' : i[1]}]]]]
-              for i in x[2]]]]
-         for x in sortimgs[::-1]]
-            
+         ['tr', ['td', x[1].replace("\n", "<br />")]],
+         ['tr', [['td', ['h2', "expected"]], ['td', ['h2', "actual"]]]],
+         [['tr',
+          [['td', ['img', {'src': i[0]}]],
+           ['td', ['img', {'src': i[1]}]]]]
+             for i in x[2]]]]
+        for x in sortimgs[::-1]]
+
 myhtml = html(data)
 
 pretty = HTMLBeautifier.beautify(myhtml, 2)
 
-f = open(TEST_PATH + 'compare.html','w')
+f = open(TEST_PATH + 'compare.html', 'w')
 f.write(pretty)
 f.close()
